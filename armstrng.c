4c95642d2ec36307d6f966830b8bf29f56ae2adb
sampathfzs <31653428+sampathfzs@users.noreply.github.com>
2017-09-19 07:23:29 -0700

    Create armstrng.c

---
+++ armstrng.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int mian()
+{
+int a,b,c,rem=0;
+scanf("%d",&a);
+c=a%10;
+a=a/10;
+rem=c*c*c;
+rem=1+rem
+while(a%10==0)
+rem1=rem-1;
+if(rem1==a)
+{
+printf("armstrong");
+}
+}
